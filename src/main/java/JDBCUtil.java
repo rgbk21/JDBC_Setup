5a2145b478929e6c6d73acc504775837d9983aa8
rgbk21 <rajgaurav.bk@gmail.com>
2020-09-21 11:38:29 -0400

    Initial Commit

---
+++ src/main/java/JDBCUtil.java
@@ -0,0 +1,100 @@
+import java.sql.*;
+
+public class JDBCUtil {
+
+    // https://stackoverflow.com/questions/4832056/java-jdbc-how-to-connect-to-oracle-using-service-name-instead-of-sid
+    public static Connection getConnection() throws SQLException{
+
+        // Register the OracleDB JDBC Driver
+        Driver d =  new oracle.jdbc.driver.OracleDriver();
+        DriverManager.registerDriver(d);
+
+        // Construct the connection URL
+        String connectionUrl = "jdbc:oracle:thin:hr/hr@//localhost:1521/orclpdb";
+        String userId = "hr";
+        String password = "hr";
+
+        // Get a connection
+        Connection conn = DriverManager.getConnection(connectionUrl, userId, password);
+
+        // Set the auto-commit to off
+        conn.setAutoCommit(false);
+
+        return conn;
+
+    }
+
+    public static void closeConnection(Connection conn) {
+
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void closeStatement(Statement statement) {
+
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void commit(Connection connection) {
+
+        try {
+            if (connection != null) {
+                connection.commit();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void rollback(Connection connection) {
+
+        try {
+            if (connection != null) {
+                connection.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        try {
+            conn = JDBCUtil.getConnection();
+            System.out.println("Connected to the database");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtil.closeConnection(conn);
+        }
+
+    }
+}
