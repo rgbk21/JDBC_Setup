0fd1b091d62b17320f74e24ed668dbbd6243da30
rgbk21 <rajgaurav.bk@gmail.com>
2020-09-26 12:43:04 -0400

    JDBCRowSetTest Added

--- src/main/java/com/example/demo/RowSet/JDBCRowSetTest.java
+++ src/main/java/com/example/demo/RowSet/JDBCRowSetTest.java
@@ -12,6 +12,54 @@ public class JDBCRowSetTest {
 
         RowSetFactory factory = RowSetUtil.getRowSetFactory();
 
+        readFromPersonTable(factory);
+        updatePersonTable(factory);
+
+    }
+
+    private static void updatePersonTable(RowSetFactory factory) {
+
+        // Use try-with-resources block
+        try (JdbcRowSet jdbcRowSet = factory.createJdbcRowSet()) {
+
+            // Set the connection parameters
+            RowSetUtil.setConnectionParameters(jdbcRowSet);
+
+            // Set the auto-commit mode to false
+            jdbcRowSet.setAutoCommit(false);
+
+            // Set the command and input parameters
+            String sqlCommand = "SELECT person_id, first_name, last_name, income " +
+                    "FROM person " +
+                    "WHERE person_id = ?";
+
+            // Set the command to the RowSet object
+            jdbcRowSet.setCommand(sqlCommand);
+            jdbcRowSet.setInt(1, 5);
+
+            // Retrieve the data
+            jdbcRowSet.execute();
+
+            // If a row is retrieved, update it's income.
+            // Note the updateDouble, updateRow, commit methods being used.
+            if (jdbcRowSet.next()) {
+                double oldIncome = jdbcRowSet.getDouble("income");
+                double newIncome = oldIncome + 1000;
+                jdbcRowSet.updateDouble("income", newIncome);
+                jdbcRowSet.updateRow();
+                jdbcRowSet.commit();
+            }
+
+            // Print the records in the rowset
+            RowSetUtil.printPersonRecord(jdbcRowSet);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readFromPersonTable(RowSetFactory factory) {
+
         // Use try-with-resources block
         try (JdbcRowSet jdbcRowSet = factory.createJdbcRowSet()) {
 
@@ -31,7 +79,8 @@ public class JDBCRowSetTest {
             // Retrieve the data
             jdbcRowSet.execute();
 
-            // Scroll to the last row to get the row count It may throw an exception if the underlying JdbcRowSet implementation
+            // Scroll to the last row to get the row count.
+            // It may throw an exception if the underlying JdbcRowSet implementation
             // does not support a bi-directional scrolling result set.
             try {
                 jdbcRowSet.last();
@@ -49,7 +98,6 @@ public class JDBCRowSetTest {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
 }
